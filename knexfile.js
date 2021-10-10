3d7be9891ecc01d04dbe51d62b835e40f4dbbf7f
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2021-10-09 23:44:58 -0300

    .env

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,20 @@
 // Update with your config settings.
-const {Pool} = require('pg')
-require('dotenv/config')
+
 
 module.exports = {
   
     client:'postgresql',
-    connection: process.env.db,
-	pool: {
-		min: 2,
-		max: 10
-	},
+    connection: {
+      database: process.env.database,
+      user: process.env.user,
+      password: process.env.password,
+      port: process.env.port,
+      host: process.env.host,
+      charset: process.env.charset,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
     migrations: {
       tableName: 'knex_migrations'
     }
