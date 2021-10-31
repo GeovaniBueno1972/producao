6e825bf044846c8c731298b54a88735846a5f8da
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2021-10-31 18:59:42 -0300

    alteracao 31 10

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const db = require('./config/db')
 
 app.db = db
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET ')
+        return res.status(200).send({})
+    }
+    next()
+})
+
 consign()
     .include('./config/passport.js')
     .then('./config/middlewares.js')
