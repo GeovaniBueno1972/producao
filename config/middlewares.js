b6a912945abbc4b00eacafacbe073888e13b71d1
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2021-10-31 18:39:42 -0300

    alteração 31 10

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,13 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
 module.exports = app => {
     app.use(bodyParser.json())
-    app.use(cors())
+    app.use(cors(corsOptions))
 }
