98f3c4cd88930d5d9388fc7c75cbd87f6b001556
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2021-11-06 16:47:24 -0300

    Alteracao 06 11

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const app = require('express')()
 const consign = require('consign')
 const db = require('./config/db')
+const cors = require('cors')
 
 app.db = db
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Header',
+    app.use(cors())
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Header",
     'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET ')
+        res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET ')
         return res.status(200).send({})
     }
     next()
