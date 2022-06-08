8c8f5ce0796fa17167ccef0e39b719a537f6d1d7
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2022-06-08 19:10:59 -0300

    teste 08/06/22

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const consign = require('./consign')
+const db = require('./config/db')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.db = db
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Header",
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS'){
+        res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET ')
+        return res.status(200).send({})
+    }
+    app.use(cors())
+    next()
+})
+
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api/validation.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Backend executando...')
+})
